c1f85c339c49287d0a03844cb69c2278f568d139
Travis Geis <travis@qualia.com>
2018-05-03 10:12:15 -0700

    Initial commit

---
+++ template/index.js
@@ -0,0 +1,39 @@
+const decamelize = require('decamelize');
+const Generator = require('yeoman-generator');
+const path = require('path');
+
+module.exports = class extends Generator {
+
+  prompting() {
+    this.context = {};
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'New template name',
+        validate: input => !!input.trim().length,
+      },
+    ]).then(answers => {
+      this.context.templateName = answers.name;
+    });
+  }
+
+  writing() {
+    const paths = [
+      'template.html',
+      'template.js',
+      'template.css',
+    ];
+
+    const baseName = decamelize(this.context.templateName, '-');
+
+    paths.forEach(inPath => {
+      const outPath = inPath.replace('template', baseName);
+      this.fs.copyTpl(
+        this.templatePath(inPath),
+        this.destinationPath(outPath),
+        this.context
+      );
+    });
+  }
+};
---
+++ template/templates/template.js
@@ -0,0 +1,22 @@
+Template.<%= templateName %>.onCreated(function() {
+  const tpl = this;
+
+  _.extend(tpl, {
+
+    initialize() {
+
+    },
+
+
+
+  }).initialize();
+
+});
+
+Template.<%= templateName %>.onDestroyed(function() {
+
+});
+
+Template.<%= templateName %>.events({
+
+});
