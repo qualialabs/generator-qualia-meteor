09b34fcb7d5fcf50c56b54e04927e0ae881e335c
Travis Geis <travis@qualia.com>
2018-09-07 12:01:56 -0700

    Switch filenames to use underscore

--- template/index.js
+++ template/index.js
@@ -25,7 +25,7 @@ module.exports = class extends Generator {
       'template.css',
     ];
 
-    const baseName = decamelize(this.context.templateName, '-');
+    const baseName = decamelize(this.context.templateName, '_');
 
     paths.forEach(inPath => {
       const outPath = inPath.replace('template', baseName);
